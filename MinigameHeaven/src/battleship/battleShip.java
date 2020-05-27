b7792143d5332b136bb8371ec5e9aad152569e85
SungyounKim <sg17990@gmail.com>
2020-05-27 16:06:08 +0900

    BattleShip Game add

---
+++ MinigameHeaven/src/battleship/battleShip.java
@@ -0,0 +1,34 @@
+package battleship;
+
+public class battleShip {
+	
+	int battleShip = 4; //size 4
+	int frigate1; // size 3
+	int frigate2; //size 3
+	int airCraftCarrier; // size 5
+	int submarine; // size 2
+	String battleShipLocation = "D4";
+	
+	
+	public static void main(String[] args) {
+		String[][] board = new String[10][10];
+		
+		
+		for(int i =0; i<10; i++) {
+			if(i == 0) {
+				System.out.print("  ");
+			}
+			System.out.print(i+1 + " ");
+		}
+		System.out.println();
+		for(int i =0; i < board.length; i++) {
+			System.out.print(i+1 +" ");
+			for(int j=0; j < board[i].length; j++) {
+
+				board[i][j] = "~";
+				System.out.print(board[i][j]+ " ");
+			}
+			System.out.println();
+		}
+	}
+}
